18581e14f595ab805302a6ce1145ca20e76e17f7
EdTriplett <EdTriplett@users.noreply.github.com>
2018-02-04 02:40:21 -0500

    q15 text

--- euler.js
+++ euler.js
@@ -684,3 +684,14 @@ let longestCollatzSequence = (maxStart) => {
 
 
 console.log(longestCollatzSequence(1000000))
+
+
+/*
+Lattice paths
+Problem 15
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+
+How many such routes are there through a 20×20 grid?
+
+*/
