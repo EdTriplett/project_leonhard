5695e482b2652e6a365780cf473ab17b7b902d31
EdTriplett <EdTriplett@users.noreply.github.com>
2018-02-24 23:15:51 -0500

    p15 factorialize

--- problem15.js
+++ problem15.js
@@ -6,9 +6,27 @@ Starting in the top left corner of a 2×2 grid, and only being able to move to t
 How many such routes are there through a 20×20 grid?
 */
 
-// (n+k)! / (k!*(n-k)!)
+// (a + b) choose b (where b is lesser one ?)  k=b, n=(a+b)
 
+// = (a+b)! / b!* (a+b-b)!
 
-//NOT passing
+// = (a+b)! / b!*a!
+
+let factorial = num => {
+  if (num % 1) {
+    console.log("error, only positive integers can be factorialized")
+    return "error, only positive integers can be factorialized"
+  }
+  if (num < 0) {
+    console.log("error, only positive integers can be factorialized")
+    return "error, only positive integers can be factorialized"
+  }
+  if (num === 0) {
+    return 1
+  }
+  else {
+    return num * factorial(num-1)
+  }
+}
 
 console.log(latticePathCount(20))
