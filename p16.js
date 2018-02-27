e536dd16dd4b257acceaa06bc3d48841fef59995
EdTriplett <EdTriplett@users.noreply.github.com>
2018-02-26 19:13:52 -0500

    initial p16

---
+++ p16.js
@@ -0,0 +1,21 @@
+/*
+Power digit sum
+Problem 16
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+*/
+
+let powerDigitSum = num => {
+  let sum = 0
+  while (num > 10) {
+    sum += num % 10
+    num = Math.floor(num/10)
+  }
+  sum += num
+  return sum
+}
+
+let input = Math.pow(2, 1000)
+
+console.log(powerDigitSum(input))
