05ab27272f4e60e58bb3e1630820f1e075b8df58
EdTriplett <EdTriplett@users.noreply.github.com>
2018-02-28 21:39:23 -0500

    >=

--- p16.js
+++ p16.js
@@ -8,7 +8,7 @@ What is the sum of the digits of the number 2^1000?
 
 let powerDigitSum = num => {
   let sum = 0
-  while (num > 10) {
+  while (num >= 10) {
     sum += num % 10
     num = Math.floor(num/10)
   }
