fe40bdb7d5ec9446f00e234d4d7d757f54697545
EdTriplett <EdTriplett@users.noreply.github.com>
2018-03-02 01:35:52 -0500

    p16 sol'n

--- euler.js
+++ euler.js
@@ -744,3 +744,46 @@ let factorial = num => {
 let southEastLatticePathCount = nodesPerSide => factorial(2 * nodesPerSide) / Math.pow(factorial(nodesPerSide), 2)
 
 console.log(southEastLatticePathCount(20))
+
+/*
+Power digit sum
+Problem 16
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+*/
+
+let stringMultiply = (numStr1, numStr2) => {
+  // takes string args, returns string
+  let digit, temp, carry = 0, product = ''
+  while (numStr1.length) {
+    digit =  Number(numStr1.slice(-1))
+    numStr1 = numStr1.slice(0, numStr1.length-1)
+    temp = digit * Number(numStr2) + carry
+    carry = Math.floor(temp/10)
+    product = (temp%10).toString().concat(product)
+  }
+  product = carry ? carry.toString().concat(product) : product
+  return product
+}
+
+let stringPowerOfTwo = num => {
+  // return a string representing 2 raised to the power of the integer num
+  let iterCount = 0, product = '1'
+  while (iterCount < num) {
+    iterCount += 1
+    product = stringMultiply(product, '2')
+  }
+  return product
+}
+
+let stringDigitSum = numStr => {
+  let sum = 0
+  while (numStr.length) {
+    sum += Number(numStr.slice(-1))
+    numStr = numStr.slice(0, numStr.length-1)
+  }
+  return sum
+}
+
+console.log(stringDigitSum(stringPowerOfTwo(1000)))
--- p16.js
+++ p16.js
@@ -6,18 +6,37 @@ Problem 16
 What is the sum of the digits of the number 2^1000?
 */
 
-let powerDigitSum = num => {
-  let sum = 0
-  while (num >= 10) {
-    sum += num % 10
-    num = Math.floor(num/10)
+let stringMultiply = (numStr1, numStr2) => {
+  // takes string args, returns string
+  let digit, temp, carry = 0, product = ''
+  while (numStr1.length) {
+    digit =  Number(numStr1.slice(-1))
+    numStr1 = numStr1.slice(0, numStr1.length-1)
+    temp = digit * Number(numStr2) + carry
+    carry = Math.floor(temp/10)
+    product = (temp%10).toString().concat(product)
   }
-  sum += num
+  product = carry ? carry.toString().concat(product) : product
+  return product
+}
+
+let stringPowerOfTwo = num => {
+  // return a string representing 2 raised to the power of the integer num
+  let iterCount = 0, product = '1'
+  while (iterCount < num) {
+    iterCount += 1
+    product = stringMultiply(product, '2')
+  }
+  return product
+}
+
+let stringDigitSum = numStr => {
+  let sum = 0
+  while (numStr.length) {
+    sum += Number(numStr.slice(-1))
+    numStr = numStr.slice(0, numStr.length-1)
+  }
   return sum
 }
 
-let input = Math.pow(2, 1000)
-// too many significant digits
-console.log('input = ', input)
-
-console.log(powerDigitSum(input))
+console.log(stringDigitSum(stringPowerOfTwo(1000)))
